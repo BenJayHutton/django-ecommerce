74e0f507cf5f5c942a3ce72cede451e1b22fafa6
Ben H <23144090+BenJayHutton@users.noreply.github.com>
2024-06-02 17:02:28 +0100

    amended static folder settings

--- src/eCommerce/settings/base.py
+++ src/eCommerce/settings/base.py
@@ -105,6 +105,9 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+STATICFILES_DIRS = [
+    BASE_DIR.parent / 'static'
+]
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
--- src/eCommerce/urls.py
+++ src/eCommerce/urls.py
@@ -15,6 +15,7 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
+from django.conf import settings
 from django.urls import include, path
 from .views import DefaultHomePage
 
@@ -24,3 +25,7 @@ urlpatterns = [
     path('product/', include('products.urls')),
     path('products/', include('products.urls')),
 ]
+
+if settings.DEBUG:
+    from django.conf.urls.static import static
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
