371c7dfe48dce1dfda0e7a4bfb4ade8444f96e2f
Ben H <23144090+BenJayHutton@users.noreply.github.com>
2024-06-18 07:07:34 +0100

    setting up api app

--- src/api/views.py
+++ src/api/views.py
@@ -1,4 +1,14 @@
+import json
 from django.http import JsonResponse
 
+from products.models import Product
+
 def api_home(request, *args, **kwargs):
-    return JsonResponse({"Message": "Json response!"})
+    model_data = Product.objects.all().order_by("?").first()
+    data = {}
+    if model_data:
+        data['title'] = model_data.title
+        data['description'] = model_data.description
+        data['price'] = model_data.price
+  
+    return JsonResponse(data)
--- src/eCommerce/urls.py
+++ src/eCommerce/urls.py
@@ -25,7 +25,7 @@ urlpatterns = [
     path('product/', include('products.urls',namespace='product')),
     path('products/', include('products.urls', namespace='products')),
     path('search/', include('search.urls', namespace='search')),
-    path('api/', include('api.urls', namespace='api') name = 'api'),
+    path('api/', include('api.urls', namespace='api')),
 ]
 
 if settings.DEBUG:
