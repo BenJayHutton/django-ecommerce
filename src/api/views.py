c278106bd9e23f4d4449281866d8cfff2b40071f
Ben H <23144090+BenJayHutton@users.noreply.github.com>
2024-06-19 18:46:27 +0100

    added serilazer.py so we can include image field and tags field in the api response

--- src/api/views.py
+++ src/api/views.py
@@ -1,6 +1,5 @@
 import json
 from django.forms.models import model_to_dict
-# from django.http import JsonResponse
 
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
@@ -10,12 +9,8 @@ from products.serializers import ProductSerializer
 
 @api_view(["GET"])
 def api_home(request, *args, **kwargs):
-    """
-    DRF API View
-    """
     instance = Product.objects.all().order_by("?").first()
     data = {}
     if instance:
-        # data = model_to_dict(instance, exclude=["image","tags"])
         data = ProductSerializer(instance).data
     return Response(data)
--- src/eCommerce/settings/base.py
+++ src/eCommerce/settings/base.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     # project app
     'api',
     'products',
+    'rest_framework',
     'search',
 ]
 
