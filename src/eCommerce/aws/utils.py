62a42078201f78f91ac427cdbabcce7d0c842f13
Ben H <23144090+BenJayHutton@users.noreply.github.com>
2024-12-08 21:33:56 +0000

    removed reference to aws, for now

--- src/eCommerce/aws/utils.py
+++
@@ -1,5 +0,0 @@
-import boto3
-
-StaticRootS3BotoStorage = None #lambda: S3Boto3Storage(location='static')
-MediaRootS3BotoStorage  = None #lambda: S3Boto3Storage(location='media')
-ProtectedS3Storage  = None #lambda: S3Boto3Storage(location='protected')
